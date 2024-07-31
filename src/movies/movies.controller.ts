5fad393878fa520f332d7119860698533ea4a725
Ahmet Didar Zafer <ahmetdidarzafer@gmail.com>
2024-07-31 11:36:16 +0300

    Added movies module

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('movies')
+@ApiTags("Movies")
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Post("createMovie")
+  create(@Body() createMovieDto: CreateMovieDto) {
+    return this.moviesService.create(createMovieDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.moviesService.findAll();
+  }
+
+  @Get('findMovie/:id')
+  findOne(@Param('id') id: string) {
+    return this.moviesService.findOne(+id);
+  }
+
+  @Patch('updateMovie/:id')
+  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
+    return this.moviesService.update(+id, updateMovieDto);
+  }
+
+  @Delete('deleteMovie/:id')
+  remove(@Param('id') id: string) {
+    return this.moviesService.remove(+id);
+  }
+}
